38b9fc41a1492c8edc285c4212230d2f8a58ed45
JangHwanPark <wkd5878@gmail.com>
2024-02-04 19:46:30 +0900

    [Leet] 26, 28

--- Programmers/lv1/86491.js
+++ Programmers/lv1/86491.js
@@ -3,11 +3,11 @@ function solution(sizes) {
     const rotated = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]);
     console.log('rotated', rotated)
     let maxSize = [0, 0];
+    console.log('maxSize', maxSize)
     rotated.forEach(([w, h]) => {
         if (w > maxSize[0]) maxSize[0] = w;
         if (h > maxSize[1]) maxSize[1] = h;
     });
-
     return maxSize[0] * maxSize[1];
 }
 
