4d9666fa04d05ce13917b4d43a8441953bc26bcc
japark <wkd5878@gmail.com>
2024-02-22 21:01:27 +0900

    [Leet] 1678

--- Leetcode/String/1678_leet.js
+++ Leetcode/String/1678_leet.js
@@ -9,15 +9,38 @@ function interpret(command) {
 }
 
 function interpretSolution(command) {
+    let parsedCommand = "";
+
+    for (let i = 0; i < command.length; i++) {
+        if (command[i] === 'G') parsedCommand = parsedCommand + 'G';
+        else if (command[i] === '(' && command[i + 1] === ')') {
+            i = i + 1;
+            parsedCommand = parsedCommand + 'o';
+        } else {
+            i = i + 3;
+            parsedCommand = parsedCommand + 'al';
+        }
+    }
+
+    return parsedCommand;
+}
+
+function interpretSolutionFn(command) {
     return command.replaceAll('()', 'o').replaceAll('(al)', 'al');
 }
 
 let command = "G()(al)";
 let command2 = "G()()()()(al)";
-let command3 = "(al)G(al)()()G"
+let command3 = "(al)G(al)()()G";
+
 console.log(interpret(command));
 console.log(interpret(command2));
 console.log(interpret(command3));
+
+console.log(interpretSolutionFn(command));
+console.log(interpretSolutionFn(command2));
+console.log(interpretSolutionFn(command3));
+
 console.log(interpretSolution(command));
 console.log(interpretSolution(command2));
 console.log(interpretSolution(command3));
