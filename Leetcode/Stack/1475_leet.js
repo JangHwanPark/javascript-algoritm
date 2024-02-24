08a44cf80b5c311c227d6f36cb416f282487d139
JangHwanPark <wkd5878@gmail.com>
2024-02-24 22:53:40 +0900

    [LeetCode] Tree and 590

---
+++ Leetcode/Stack/1475_leet.js
@@ -0,0 +1,26 @@
+function finalPrices(prices) {
+    let answer = [];
+
+    for (let i = 0; i < prices.length; i++) {
+        
+    }
+}
+
+/*
+ * Input: prices = [8,4,6,2,3]
+ * Output: [4,2,4,2,3]
+ */
+let prices = [8,4,6,2,3];
+console.log(finalPrices(prices));
+/*
+ * Input: prices2 = [1,2,3,4,5]
+ * Output: [1,2,3,4,5]
+ */
+let prices2 = [1,2,3,4,5];
+console.log(finalPrices(prices2));
+/*
+ * Input: prices3 = [10,1,1,6]
+ * Output: [9,0,1,6]
+ */
+let prices3 = [1,2,3,4,5];
+console.log(finalPrices(prices3));
